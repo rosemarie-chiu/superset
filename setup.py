3c6b8314b074718e4e4fdedfa1e0071405b0d8ed
Daniel Gaspar <danielvazgaspar@gmail.com>
2020-10-26 11:22:08 +0000

    test FAB 3.1.1rc2

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
         "croniter>=0.3.28",
         "cryptography>=2.4.2",
         "flask>=1.1.0, <2.0.0",
-        "flask-appbuilder==3.1.1rc1",
+        "flask-appbuilder==3.1.1rc2",
         "flask-caching",
         "flask-compress",
         "flask-talisman",
